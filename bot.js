c1c5970ff8499cfb05e615b78cc72fa6f5974751
Ysfkynr123 <43915905+Ysfkynr123@users.noreply.github.com>
2018-10-16 16:22:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -159,30 +159,31 @@ client.on('message', msg => {
   const args = msg.content.slice(prefix.length).split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  if(command ==="sil") {
-    let cont = msg.content.slice(prefix.length).split(" ");
-    let args = cont.slice(1);
 
-    async function purge() {
-      let fetched = await msg.channel.fetchMessages({limit: args[0]});
+    if(command ==="sil") {
+      let cont = msg.content.slice(prefix.length).split(" ");
+      let args = cont.slice(1);
 
-    if(!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.channel.send(`:no_entry: Geçersiz İşlem! \n Mesaj Silme Yetkin YOK **${msg.author.username}**`)
-      if (isNaN(args[0])) {
-        msg.channel.send("Lutfen Bir Kelime Belirtin! \n Örnek = !sil `2`");
-        return;
-      }
-    msg.channel.bulkDelete(fetched)
-    .catch(error =>  msg.channel.send(`Hata: ${error}`));
+      async function purge() {
+        let fetched = await msg.channel.fetchMessages({limit: args[0]});
 
-    //https://www.youtube.com/watch?v=Zpxyio10Kj0
-    msg.channel.bulkDelete(args[0]).then(() => {
-      msg.channel.send(`:white_check_mark: Başarılı İşlem ! \n :gear: **${args[0]}** Mesaj Silindi! Silen Kişi = **${msg.author.username}**`)
-    });
+      if(!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.channel.send(`:no_entry: Geçersiz İşlem! \n Mesaj Silme Yetkin YOK **${msg.author.username}**`)
+        if (isNaN(args[0])) {
+          msg.channel.send("Lutfen Bir Kelime Belirtin! \n Örnek = !sil `2`");
+          return;
+        }
+      msg.channel.bulkDelete(fetched)
+      .catch(error =>  msg.channel.send(`Hata: ${error}`));
+
+      //https://www.youtube.com/watch?v=Zpxyio10Kj0
+      msg.channel.bulkDelete(args[0]).then(() => {
+        msg.channel.send(`:white_check_mark: Başarılı İşlem ! \n :gear: **${args[0]}** Mesaj Silindi! Silen Kişi = **${msg.author.username}**`)
+      });
 //\n
-  }
-  purge();
+    }
+    purge();
 
-}
+  }
   if(command === "temizle") {
       let cont = msg.content.slice(prefix.length).split(" ");
       let args = cont.slice(1);
@@ -216,7 +217,7 @@ client.on('message', msg => {
       msg.channel.send(botmessage);
       //// +  `\n**Mesaj Sahibi = ${msg.author.username}**`
     }
-    if(msg.content.toLowerCase() === prefix + "say") {
+    if(command === "say") {
       if(!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.reply("Yetkin Yok!");
       let botmessage = args.join(" ");
         msg.delete();
@@ -226,7 +227,7 @@ client.on('message', msg => {
     if(command === "sus") {
       let mUser = msg.guild.member(msg.mentions.users.first () || msg.guild.members.get(args[0]));
       let mlg = msg.guild.channels.find("name", "mod-log")
-      if(!mUser) msg.reply("Kullanıcı Bulunamadı!")
+if(!mUser) return msg.channel.send("Bu Komutu Kullanmak İçin **[!sus @Kullanıcı#1233 ]** Yazmalısın!")
 
       mUser.addRole(mUser.guild.roles.find("name", "mute"));
       mlg.send(`**<@${msg.author.id}>** , **${mUser}** Adlı Kişiyi **Sus**turdu`)
@@ -235,8 +236,8 @@ client.on('message', msg => {
      if(command === "konuş") {
       let mUser = msg.guild.member(msg.mentions.users.first () || msg.guild.members.get(args[0]));
       let mlg = msg.guild.channels.find("name", "mod-log")
-      if(!mUser) msg.reply("Kullanıcı Bulunamadı!")
-
+if(!mUser) return msg.channel.send("Bu Komutu Kullanmak İçin **[!konuş @Kullanıcı#1233 ]** Yazmalısın!")
+       
       mUser.removeRole(mUser.guild.roles.find("name", "mute"));
       mlg.send(`**<@${msg.author.id}>** , **${mUser}** Adlı Kişinin **Sus**turma Engelini **Kaldırdı**`)
       msg.channel.send(`**<@${msg.author.id}>** , **${mUser}** Adlı Kişinin **Sus**turma Engelini **Kaldırdı**`)
@@ -255,210 +256,38 @@ client.on('message', msg => {
     if(command === "mute") {
       let mUser = msg.guild.member(msg.mentions.users.first () || msg.guild.members.get(args[0]));
       let mlg = msg.guild.channels.find("name", "mod-log")
-      if(!mUser) msg.reply("Kullanıcı Bulunamadı!")
-
+      if(!mUser) return msg.channel.send("Bu Komutu Kullanmak İçin **[!mute @Kullanıcı#1233 ]** Yazmalısın!")
+      
       mUser.addRole(mUser.guild.roles.find("name", "mute"));
       mlg.send(`**<@${msg.author.id}>** , **${mUser}** Adlı Kişiyi **Sus**turdu`)
       msg.channel.send(`**<@${msg.author.id}>** , **${mUser}** Adlı Kişiyi **Sus**turdu`)
      }
-     if(command === "unmute") {      let mUser = msg.guild.member(msg.mentions.users.first () || msg.guild.members.get(args[0]));
+     if(command === "unmute") {      
+      let mUser = msg.guild.member(msg.mentions.users.first () || msg.guild.members.get(args[0]));
       let mlg = msg.guild.channels.find("name", "mod-log")
-      if(!mUser) msg.reply("Kullanıcı Bulunamadı!")
+      if(!mUser) return msg.channel.send("Bu Komutu Kullanmak İçin **[!unmute @Kullanıcı#1233]** Yazmalısın!")
 
       mUser.removeRole(mUser.guild.roles.find("name", "mute"));
       mlg.send(`**<@${msg.author.id}>** , **${mUser}** Adlı Kişinin **Sus**turma Engelini **Kaldırdı**`)
       msg.channel.send(`**<@${msg.author.id}>** , **${mUser}** Adlı Kişinin **Sus**turma Engelini **Kaldırdı**`)
     }
-    client.on('message', msg => {
-  if(msg.content.toLowerCase === 'discord.gg') {
-   msg.delete(30)
-    msg.reply('Bu Discord Benim Korumam Altında Ve Reklam Yapman Yasak!');
+    if (command === "profil")
+    if (msg.channel.type !== "group") {
+        var Durum = msg.author.presence.status;
+        var Durm = (Durum == "online" ? (0x00AE86) : (Durum == "offline" ? (0x808080) : (Durum == "idle" ? (0xFFFF00) : (Durum == "dnd" ? (0xFF0000) : (0x00AE86)))))
+        var durm = (Durum == "online" ? ("Çevrimiçi") : (Durum == "offline" ? ("Çevrimdışı") : (Durum == "idle" ? ("Boşta") : (Durum == "dnd" ? ("Rahatsız Etmeyin") : ("Bilinmiyor/bulunamadı.")))))
+      const kullanicibilgimk = new Discord.RichEmbed()
+      .setAuthor(msg.author.username, msg.author.avatarURL)
+      .setColor(Durm)
+      .setTimestamp()
+      .addField('Kullanıcı İsmin:', msg.author.username + '#' + msg.author.discriminator)
+      .addField('ID:', msg.author.id)
+      .addField('Discord Uygulamasına Kayıt tarihi:', msg.author.createdAt)
+      .addField('Durum:', durm)
+      .addField('Şu an oynadığı oyun:', msg.author.presence.game ? msg.author.presence.game.name : 'Şu an oyun oynamıyor')
+      .addField('BOT mu?', msg.author.bot ? '\n Evet' : 'Hayır')
+      return msg.channel.sendEmbed(kullanicibilgimk);
   }
-});
-    if(command === 'ban') {
-      let bUser = msg.guild.member(msg.mentions.users.first () || msg.guild.members.get(args[0]));
-      if(!bUser) return msg.channel.send("Bu Komutu Kullanmak İçin **[!ban @Kullanıcı#1233 Sebep]** Yazmalısın!")
-      let bReason = args.join(" ").slice(22);
-      //if (msg.author.id !== "346789430012411904") return
-      let bEmbed = new Discord.RichEmbed()
-       .setDescription("Banlanan Kullanıcı!")
-       .setColor("#e56b00")
-       .addField("Banlanan Kullanıcı", `**${bUser}** Banlanan Kullanıcı ID Si **${bUser.id}**`)
-       .addField("Banlayan Kişi", `**<@${msg.author.id}>** Banlayan Kişi ID Si **${msg.author.id}**`)
-       .addField("Banlanma Zamanı", msg.createdAt)
-       .addField("Banlanma Nedeni", bReason);
-
-       let banChannel = msg.guild.channels.find(`name`, `mod-log`)
-       if(!banChannel) return msg.channel.send("mod-log Yazı Kanalı Bulunmadı")
-
-       msg.guild.member(bUser).ban(bReason)
-       banChannel.send(bEmbed);
-       return;
-       //https://www.youtube.com/watch?v=ukiVc8FSSpY&list=PLdnyVeMcpY7-GfaXaWBOb3ZQkJxP53BIx&index=4
-   }
-
-   if(command === 'at') {
-       let kUser = msg.guild.member(msg.mentions.users.first () || msg.guild.members.get(args[0]));
-       if(!kUser) return msg.channel.send("Bu Komutu Kullanmak İçin **[!at @Kullanıcı#1233 Sebep]** Yazmalısın! ")
-       let kReason = args.join(" ").slice(22);
-       if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send("Bu Uyeyi Atmaya İznin YOK")
-
-
-       let kEmbed = new Discord.RichEmbed()
-        .setDescription("Atılan Kullanıcı!")
-        .setColor("#e56b00")
-        .addField("Atılan Kullanıcı", `${kUser} Atılan Kullanıcı ID Si **${kUser.id}**`)
-        .addField("Atan Kişi", `<@${msg.author.id}> Atan Kişi ID Si **${msg.author.id}**`)
-        .addField("Atılma Zamanı", msg.createdAt)
-        .addField("Atılma Nedeni", kReason);
-
-        let kickChannel = msg.guild.channels.find(`name`, `mod-log`)
-        if(!kickChannel) return msg.channel.send("mod-log Yazı Kanalı Bulunmadı")
-
-        msg.guild.member(kUser).kick(kReason)
-        kickChannel.send(kEmbed);
-        return;
-        //https://www.youtube.com/watch?v=ukiVc8FSSpY&list=PLdnyVeMcpY7-GfaXaWBOb3ZQkJxP53BIx&index=4
-    }
-     if(command === "report") {
-    let rUser = msg.guild.member(msg.mentions.users.first() || msg.guild.members.get(args[0]));
-    if(!rUser) return msg.channel.send("Bu Komutu Kullanmak İçin **[!report @Kullanıcı#1233 Sebep]** Yazmalısın! ")
-    let rreason = args.join(" ").slice(22);
-    if(!rreason) return msg.channel.send("Lütfen **Bir Sebep Belirt**.")
-
-let reportEmbed = new Discord.RichEmbed()
-.setDescription("Reports")
-.setColor("#15f153")
-.addField("Reportlanan Kullanıcı", `${rUser} ID: ${rUser.id}`)
-.addField("Reportlayan", `${msg.author} ID: ${msg.author.id}`)
-.addField("Kanal", msg.channel)
-.addField("Reportlanma Zamanı", msg.createdAt)
-.addField("Sebep", rreason);
-
-let reportschannel = msg.guild.channels.find(`name`, "mod-log");
-if(!reportschannel) return msg.channel.send("#mod-log Kanalı Bulunamadı!");
-
-
-msg.delete().catch(O_o=>{});
-reportschannel.send(reportEmbed);
-
-}
-
-     if(command === 'kick') {
-      let kUser = msg.guild.member(msg.mentions.users.first () || msg.guild.members.get(args[0]));
-      if(!kUser) return msg.channel.send("Bu Komutu Kullanmak İçin **[!kick @Kullanıcı#1233 Sebep]** Yazmalısın! ")
-      let kReason = args.join(" ").slice(22);
-      if(!msg.member.hasPermission("KICK_MEMBERS")) return msg.channel.send("Bu Uyeyi Atmaya İznin YOK")
-
-
-      let kEmbed = new Discord.RichEmbed()
-       .setDescription("Atılan Kullanıcı!")
-       .setColor("#e56b00")
-       .addField("Kicklenen Kullanıcı", `${kUser} Kicklenen Kullanıcı ID Si **${kUser.id}**`)
-       .addField("Kickleyen Kişi", `<@${msg.author.id}> Kickeleyen Kişi ID Si **${msg.author.id}**`)
-       .addField("Kickleme Zamanı", msg.createdAt)
-       .addField("Kicklenme Nedeni", kReason);
-
-       let kickChannel = msg.guild.channels.find(`name`, `mod-log`)
-       if(!kickChannel) return msg.channel.send("mod-log Yazı Kanalı Bulunmadı")
-
-       msg.guild.member(kUser).kick(kReason)
-       kickChannel.send(kEmbed);
-       return;
-       //https://www.youtube.com/watch?v=ukiVc8FSSpY&list=PLdnyVeMcpY7-GfaXaWBOb3ZQkJxP53BIx&index=4
-   }
-
-    if(command === 'yardım') {
-      let embed = new Discord.RichEmbed()
-      .setAuthor(msg.author.username)
-      .setDescription("**!Yardım Komutuna Göre ;**", true)
-
-
-      .addField("**!yardım**", "Botla İlgili Komutları Gösterir")
-
-      .addField("**!sunucubilgi**", "Sunucu İle Bilgiler Gosterir", true)
-
-      .addField("**!botbilgi**", "Bot İle İlgili Bilgiler Verir", true)
-
-      .addField("**!temizle**", "Temizle Ve Bir Sayı Yazınca Mesajlar Silinir!!", true)
-
-      .addField("**!yaz**", "!yaz Yazdıktan Sonra Bir Mesaj Belirt Ve Yetkin Varsa O Mesajı Bot Yazsın!", true)
-
-      .addField("**!at**", "!at @{nick} {sebep} Belirtilen Kisiyi Sunucuda Bir Nedenden dolayı Kickler", true)
-
-      .addField("**!ban**", "!ban @{nick} {sebep} Belirtilen Kisiyi Sunucuda Bir Nedenden dolayı BANLAR", true)
-
-      .addField("**!sus**", "mute Adlı Rol Oluşturmak Şartı İle Kişiye Mute Atar")
-
-      .addField("**!konuş**", "mute Adlı Rol Oluşturması Şartı İle Susturulan Kişinin Susutrma Engelini Kaldırır")
-
-      .addField("Diğer Şeyler", "Bot Küfür Engeller Yeni Üye Mesajı Vb. Mesajlar Vardır ")
-
-
-      .setThumbnail(msg.author.avatarURL, true)
-
-      .setColor(0xff0000)
-      return msg.channel.sendEmbed(embed);
-    }
-
-    if(command === "resim") {
-      let message = msg.channel.send("Profil Fotoğrafı Yollanıyor...");
-      let target = msg.mentions.users.first() || msg.author;
-        msg.channel.send({files: [
-          {
-            attachment: target.displayAvatarURL,
-            name: "resim.png"
-          }
-        ]});
-    }
-
-    if(command === "sunucubilgi") {
-         let embed = new Discord.RichEmbed()
-           .setAuthor(msg.author.username)
-           .setColor("#9B59B4")
-           .addField("Sunucu İsmi", msg.guild.name, true)
-           .addField("Sunucu ID", msg.guild.id, true)
-           .addField("Sunucu Kurucusu", msg.guild.owner)
-           .addField("Sunucu Kişi Sayısı", msg.guild.memberCount, true)
-           .addField("Oluşturulma Tarihi", msg.guild.createdAt)
-           .addField("AFK Kanalı", msg.guild.afkChannel,true)
-           .addField("AFK Kalma Suresi", msg.guild.afkTimeout + " Saniye")
-           .setThumbnail(msg.guild.iconURL)
-           //.setThumbnail(msg.author.avatarURL)
-
-      return msg.channel.sendEmbed(embed);
-    }
-
-       if (command === "invite") {
-         let embed = new Discord.RichEmbed()
-
-             .setDescription(`https://discordapp.com/oauth2/authorize?client_id=406095553928101890&scope=bot&permissions=2146958591`)
-
-          msg.channel.sendEmbed(embed);
-       }
-      if(command === "botbilgi") {
-        let embed = new Discord.RichEmbed()
-
-        .addField("Kurucu", "@Yusuf • ☾✰#8331", true)
-
-        .addField("Kitaplık Türü", "Java Script/Discord.js")
-
-        .addField("Version", "11.3.2", true)
-
-        .addField("**Bot Komutları**", "**!Yardım** Yazarak Öğrenebilirsiniz!")
-
-        .addField("**Bottaki Komut Sayısı**", "11")
-
-        .addField("Botu Eklemek İçin", " https://discordapp.com/oauth2/authorize?client_id=406095553928101890&scope=bot&permissions=2146958591")
-
-        .setThumbnail("https://cdn.discordapp.com/attachments/429626059202232320/429930299867004928/resim.png")
-
-
-        .setColor(0xff0000)
-
-      msg.channel.sendEmbed(embed);
-      }
-
 });
 
 client.login(process.env.bot);
