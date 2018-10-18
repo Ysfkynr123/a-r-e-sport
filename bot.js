6c1596b959f49fc1321d50837142e03b744d15e2
Ysfkynr123 <43915905+Ysfkynr123@users.noreply.github.com>
2018-10-18 15:53:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -359,53 +359,6 @@ msg.delete().catch(O_o=>{});
 reportschannel.send(reportEmbed);
 
 }
-  if(command === "kilit") {
-      const ozelmesajuyari = new Discord.RichEmbed()
-  .setColor(0xFF0000)
-  .setTimestamp()
-  .setAuthor(msg.author.username, msg.author.avatarURL)
-  .addField(':warning: Uyarı :warning:', '`kilit` adlı komutu özel mesajlarda kullanamazsın.')
-  return msg.author.sendEmbed(ozelmesajuyari); }
-  if (!client.lockit) client.lockit = [];
-  let time = args.join(' ');
-  let validUnlocks = ['release', 'unlock'];
-  if (!time) return msg.reply('Doğru kullanım: ' + prefix + 'kilit <süre örneğin: 2 min>');
-
-  if (validUnlocks.includes(time)) {
-    msg.channel.overwritePermissions(msg.guild.id, {
-      SEND_msgS: null
-    }).then(() => {
-      msg.channel.sendmsg('Kanal kilidi açıldı.');
-      clearTimeout(client.lockit[msg.channel.id]);
-      delete client.lockit[msg.channel.id];
-    }).catch(error => {
-      console.log(error);
-    });
-  } else {
-    msg.channel.overwritePermissions(msg.guild.id, {
-      SEND_msgS: false
-    }).then(() => {
-      msg.channel.sendmsg(`Kanal kilitlendi. ${ms(ms(time), { long:true })}`).then(() => {
-
-        client.lockit[msg.channel.id] = setTimeout(() => {
-          msg.channel.overwritePermissions(msg.guild.id, {
-            SEND_msgS: null
-          }).then(msg.channel.sendmsg('Kanalın kilidi açıldı.')).catch(console.error);
-          delete client.lockit[msg.channel.id];
-        }, ms(time));
-
-      }).catch(error => {
-        console.log(error);
-      });
-    });
-  }
-exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: ['ld'],
-  permLevel: 2
-};
-
      if(command === 'kick') {
       let kUser = msg.guild.member(msg.mentions.users.first () || msg.guild.members.get(args[0]));
       if(!kUser) return msg.channel.send("Bu Komutu Kullanmak İçin **[!kick @Kullanıcı#1233 Sebep]** Yazmalısın! ")
