a241a41b153871e51cd65e1c485863330cdd1135
Ysfkynr123 <43915905+Ysfkynr123@users.noreply.github.com>
2018-11-17 18:35:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -213,37 +213,6 @@ client.on('message', msg => {
     purge();
 
   }
-if(command === "sayı") {
-let mapping = {
-    ' ': '   ',
-    '0': ':zero:',
-    '1': ':one:',
-    '2': ':two:',
-    '3': ':three:',
-    '4': ':four:',
-    '5': ':five:',
-    '6': ':six:',
-    '7': ':seven:',
-    '8': ':eight:',
-    '9': ':nine:',
-    '!': ':grey_exclamation:',
-    '?': ':grey_question:',
-    '#': ':hash:',
-    '*': ':asterisk:'
-};
-
-'abcdefghijklmnopqrstuvwxyz'.split('').forEach(c => {
-	mapping[c] = mapping[c.toUpperCase()] = `:regional_indicator_${c}:`;
-});
-
-
-exports.run = function(client, message, args) {
-
-	if (args.length < 1) return message.reply('Lütfen bir mesaj belirt. **Doğru Kullanım**: ?emojiyazı <mesaj>')
-		
-	message.channel.send(args.join(' ').split('').map(c => mapping[c] || c).join(' '));
-
-});
   if(command === "yaz") {
     if(!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.reply("Yetkin Yok!");
     let botmessage = args.join(" ");
